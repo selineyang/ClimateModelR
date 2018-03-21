c3267a54e902aabd351e04bad4da52a375c0d2bb
Seline <selineyang93@gmail.com>
2018-03-21 01:50:01 -0400

    find the average temperature time series in the US
    use array to store all the temperature, and find the mean though the US, apply time series for average temperature

--- read in model example.R
+++ read in model example.R
@@ -31,8 +31,8 @@ for (i in 2:length(end)){
 }
 
 dim(tasM)
-#change kelvin to celcius
-celcius<-tasM-273.15
+#change kelvin to celsius
+celsius<-tasM-273.15
 
 nc=nc_open(files[1]) 
 lat=ncvar_get(nc, "lat")
@@ -48,11 +48,11 @@ lon=lon2
 rm(lon2)
 
 #temp in Jan
-quilt.plot(expand.grid(lon, lat),c(celcius[,,1]))
+quilt.plot(expand.grid(lon, lat),c(celsius[,,1]))
 map("world",lwd=2,add=T)
 
 #90 years after , temperature in the US
-quilt.plot(expand.grid(lon, lat),c(celcius[,,(90*12 + 1)]),xlim=c(-130,-60),ylim=c(25,50))
+quilt.plot(expand.grid(lon, lat),c(celsius[,,(90*12 + 1)]),xlim=c(-130,-60),ylim=c(25,50))
 map("state",lwd=2,add=T)
 plot(expand.grid(lon,lat), xlim=c(-128,-66),ylim=c(25,50),xlab="lon", ylab="lat")
 map("state", add=T)
@@ -63,10 +63,10 @@ map("state",regions = "North Carolina", add=T)
 points(x=-80.2442, y=36.2442, col="red",pch=16)#Winston Salem
 
 #time series
-plot(celcius[90,40,], type="l") 
+plot(celsius[90,40,], type="l") 
 
 ## which grid cell covers W-S?  plug in for first 2 coordinates, make time plot
-plot(celcius[112,63,], type="l", main = "Temperature of Winston-Salem")
+plot(celsius[112,63,], type="l", main = "Temperature of Winston-Salem")
 
 
 #task2/14/2018:
@@ -74,15 +74,15 @@ plot(celcius[112,63,], type="l", main = "Temperature of Winston-Salem")
 #draw map of NC, plot points at the lon lat us grid cells!!!
 #
 par(mfrow=c(4,2))
-plot(celcius[112,64,], type="l",main = "1") 
-plot(celcius[113,64,], type="l",main = "2") 
-plot(celcius[114,64,], type="l",main = "3") 
-plot(celcius[111,63,], type="l",main = "4") 
-plot(celcius[112,63,], type="l",main = "5") 
-plot(celcius[113,63,], type="l",main = "6") 
-plot(celcius[114,63,], type="l",main = "7") 
+plot(celsius[112,64,], type="l",main = "1") 
+plot(celsius[113,64,], type="l",main = "2") 
+plot(celsius[114,64,], type="l",main = "3") 
+plot(celsius[111,63,], type="l",main = "4") 
+plot(celsius[112,63,], type="l",main = "5") 
+plot(celsius[113,63,], type="l",main = "6") 
+plot(celsius[114,63,], type="l",main = "7") 
 
-
+dev.off()
 #Colorado
 plot(expand.grid(lon,lat), xlim=c(-110,-100),ylim=c(35,42),xlab="lon", ylab="lat", main="Colorado")
 map("state",regions = "Colorado", add=T)
@@ -90,11 +90,12 @@ map("state",regions = "Colorado", add=T)
 par(mfrow=c(3,3))
 for (i in 64:66){
   for (j in 101:103){
-    plot(celcius[j,i,], type="l")
+    plot(celsius[j,i,], type="l")
   }
 }
 
-#find the index
+dev.off()
+#find the index for certain bound
 which(lon > -120 & lon < -80)
 
 #nc=nc_open(files[1]) 
@@ -112,9 +113,8 @@ which(lon > -120 & lon < -80)
 #tas<-abind(tas1,tas2,along = 3)
 
 
-expand(lon,lat)
-
-plot(expand.grid(lon,lat))
+#expand(lon,lat)
+#plot(expand.grid(lon,lat))
 
 glbmatrix=as.data.frame(expand.grid(lon,lat))
 plot(glbmatrix)
@@ -124,13 +124,16 @@ M
 dim(M)
 
 #Extract US from the matrix
+#find the bound of US and label all in the bound as 1
 for(row in 59:70) {
   for(col in 95:117) {
     M[row, col] = 1
   }
 }
+#print out the rectangel region
 M[c(59:70),c(95:117)]
 
+#label those not in US as 0 
 M[70,c(108:117)] = 0
 M[69,c(111:116)] = 0
 M[68,c(112:114)] = 0
@@ -144,6 +147,7 @@ M[61,c(95:99,113:117)] = 0
 M[60,c(95:102,110,111,113:117)] = 0
 M[59,c(95:104,106:111,113:117)] = 0
 
+#print out new matrix 
 M[c(59:70),c(95:117)]
 
 
@@ -162,21 +166,20 @@ for (i in 95:117){
   }
 }
 
-#while ( new_M<-which (M[,]==1) ){}
+
 ##-----------------------------------------
-#color us green
+
 plot(expand.grid(lon,lat), xlim=c(-128,-66),ylim=c(25,50),xlab="lon", ylab="lat")
 map("usa", add=T)
 
+#find x and y for M where the value equal 1
 oneM<-which(M==1, arr.ind = T)
 
 #plot the us region "green" using matrix oneM
 points(x=lon[oneM[,2]],y=lat[oneM[,1]],col="green",pch=16)
 
 
-
 # this one should work, but somthing wrong 
-
 #function(find_1) =find_1(x)
 
 ##-------------------------------------------
@@ -185,11 +188,59 @@ head(usa)
 length(usa$long)
 summary(usa)
 
+##----------------------------------------------------------------
+#find average temperature time series of the US 
+dim(celsius)
+oneM[2,1]
+oneM[2,2]
+dim(oneM)
+#celsius[lon, lat, month]
+#lon=oneM[,2], lat=oneM[,1]
+plot(celsius[oneM[,2][1],oneM[,1][1],], type="l")
 
-#if(lon[95] %in% usa$long)
- # foo()
-#m = map("usa")
-#m = map("usa", fill = TRUE, plot = FALSE)
+#find the average temp for month1
+for (i in 1:length(oneM[,2])){
+  for (j in 1:length(oneM[,1])){
+    month1[i]<-celsius[oneM[,2][i], oneM[,1][j],1]
+  }
+}
+month1
+mean(month1)
 
-#point.in.polygon(point.x, point.y, m.x, m.y, mode.checked=FALSE)
+#now the problem is to how to loop through 1140 month, 3 for loop? It took a while though 
+store<-array(0,c(1140,190))
 
+for (i in 1:length(oneM[,2])){
+  for (j in 1:length(oneM[,1])){
+    store[1,i]<-cbind(celsius[oneM[,2][i], oneM[,1][j],1])
+  }
+}
+
+#store all the temperature into matrix 'store'
+for (h in 1:1140){
+  for (i in 1:length(oneM[,2])){
+    for (j in 1:length(oneM[,1])){
+      store[h,i]<-cbind(celsius[oneM[,2][i], oneM[,1][j],h])
+    }
+  }
+}
+
+#find the mean temperature for each month
+avgtemp=vector()
+for (i in 1:1140){
+  avgtemp[i]<-mean(store[i,])
+}
+avgtemp
+#time series for average temperature
+plot(avgtemp, type="l")
+
+
+
+
+#sum=0
+#for (i in 1:190){
+  #sum=sum+month1[i]0
+#}
+dim(oneM)
+length(oneM[,1])
+oneM
